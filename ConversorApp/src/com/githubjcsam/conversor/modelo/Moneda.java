c4a8a7bba28cc255106b3a0b105b3fd83d1b302a
jcsamgit <jcv.gerald@gmail.com>
2026-03-04 18:45:10 -0300

    conversorService y Moneda.java
    se agrego los codigos de las diferentes divisas en el archivo moneda.java (enum) ya que las divisas al venir de una api son inmutables! y tambien se agrego la logica para consumir la API, y convertir las diferentes monedas segun la informacion obtenida desde el servidor... solo falta agregar el metodo en el archivo Principal para empezar las pruebas...

--- ConversorApp/src/com/githubjcsam/conversor/modelo/Moneda.java
+++ ConversorApp/src/com/githubjcsam/conversor/modelo/Moneda.java
@@ -1,4 +1,19 @@
 package com.githubjcsam.conversor.modelo;
 
 public enum Moneda {
+
+    USD ("USD"),
+    ARS ("ARS"),
+    BRL ("BRL"),
+    COP ("COP");
+
+    private final String codigo;
+
+    Moneda(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
 }
--- ConversorApp/src/com/githubjcsam/conversor/service/ConversorService.java
+++ ConversorApp/src/com/githubjcsam/conversor/service/ConversorService.java
@@ -1,4 +1,58 @@
 package com.githubjcsam.conversor.service;
 
+import com.githubjcsam.conversor.modelo.Moneda;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConversorService {
+    private static final String API_KEY = "781800f1a7e03848d3b46784";
+    private static final String URL_BASE = "https://v6.exchangerate-api.com/v6";
+
+    private final HttpClient client;
+
+    public  ConversorService(){
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public double convertir (Moneda base, Moneda destino, double cantidad) throws IOException, InterruptedException {
+
+        String url = URL_BASE + API_KEY + "/lasted/" + base.getCodigo();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200){
+            throw new RuntimeException("Error en la API: " + response.statusCode());
+        }
+
+        JsonObject jsonObject = JsonParser.parseString(response.body()).getAsJsonObject();
+        JsonObject rates = jsonObject.getAsJsonObject("Conversion_rates");
+
+        double tasa = rates.get(destino.getCodigo()).getAsDouble();
+        return cantidad * tasa;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
