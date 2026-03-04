381b67b12fc546ffbe23a5731f4e79fe2d2af386
jcsamgit <jcv.gerald@gmail.com>
2026-03-04 17:39:21 -0300

    interfaz grafica de consola
    Se agrego la logica necesaria para imprimir la interfaz basica en la consola... (la unica opcion que funnciona por ahora es el 7)

--- ConversorApp/src/com/githubjcsam/conversor/Principal.java
+++ ConversorApp/src/com/githubjcsam/conversor/Principal.java
@@ -1,4 +1,46 @@
 package com.githubjcsam.conversor;
 
+import com.githubjcsam.conversor.service.ConversorService;
+
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ConversorService service = new ConversorService();
+
+        int opcion = 0;
+
+
+        while (opcion != 7){
+            mostrarMenu();
+            opcion = scanner.nextInt();
+
+            if (opcion == 7){
+                System.out.println("Finalizo el programa");
+                break;
+            }
+
+            System.out.println("Ingrese la cantidad: ");
+            double cantidad = scanner.nextDouble();
+
+
+        }
+
+    }
+
+    private static void mostrarMenu(){
+        System.out.println("""
+                ****************************
+                Conversor de monedas!
+                1) Dolar >>> Peso Argentino
+                2) Peso >>> Argentino Dolar
+                3) Dolar >>> Real
+                4) Real >>> Dolar
+                5) Dolar >>> Peso colombiano
+                6) Peso >>> colombiano Dolar
+                7) Salir
+                """);
+        System.out.println("Elija una opcion valida: ");
+    }
 }
